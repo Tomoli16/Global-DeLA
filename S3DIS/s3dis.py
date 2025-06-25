6c670c604a07bc9f329340e6fd95e41b573f0e01
tosa098h <tosa098h@c1.capella.hpc.tu-dresden.de>
2025-06-25 13:35:56 +0200

    removed random permutation after grid subsampling

--- S3DIS/s3dis.py
+++ S3DIS/s3dis.py
@@ -107,12 +107,6 @@ class S3DIS(Dataset):
         height = xyz[:, 2:]
         feature = torch.cat([col, height], dim=1)
 
-        # Teste Auswirkung von rand Permutation
-        perm = torch.randperm(xyz.size(0), device=xyz.device)
-        xyz     = xyz[perm]
-        feature = feature[perm]
-        lbl     = lbl[perm]
-
         indices = []
         self.knn(xyz, self.grid_size[::-1], self.k[::-1], indices)
 
