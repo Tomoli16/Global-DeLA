f6d6be627150bd98f13a3f77c82b2371984d7ff6
Tom Sanchez <tosa098h@login2.barnard.hpc.tu-dresden.de>
2025-07-18 14:51:10 +0200

    added logic if pts is not given (for batch size 1)

--- utils/transforms.py
+++ utils/transforms.py
@@ -8,6 +8,8 @@ from typing import List, Optional, Tuple
 
 
 def serialization(xyz, feat, x_res=None, order="z", pts=None, layers_outputs=[], grid_size=0.02):
+    if pts is None:
+        pts = [xyz.shape[0]]
     if not isinstance(order, list):
         order = [order]
 
@@ -38,6 +40,7 @@ def serialization(xyz, feat, x_res=None, order="z", pts=None, layers_outputs=[],
 
     for i in range(len(layers_outputs)):
         layers_outputs[i] = layers_outputs[i][order]
+    print("First loop done")
     return xyz, feat, x_res, inverse_order
 
 def deserialization(
